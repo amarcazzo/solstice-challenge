26d8a17e0753c054208bf7fd5cf5bc84f6e18e24
Ayrton Marcazzo <ayrtonmarcazzo@gmail.com>
2019-05-13 16:42:05 -0300

    Updates

---
+++ precache-manifest.a86c4cc7f2337068d93aaf4fff07aaf1.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d2df21e9a2c38975b4f4b820bf8ad40a",
+    "url": "/solstice-challenge/index.html"
+  },
+  {
+    "revision": "4077197726dbb1b25b75",
+    "url": "/solstice-challenge/static/css/main.63f227a0.chunk.css"
+  },
+  {
+    "revision": "7bce3649ff57a41034c0",
+    "url": "/solstice-challenge/static/js/2.f7353d4b.chunk.js"
+  },
+  {
+    "revision": "4077197726dbb1b25b75",
+    "url": "/solstice-challenge/static/js/main.ff61bef7.chunk.js"
+  },
+  {
+    "revision": "b4d8edfaf7edf2e6277c",
+    "url": "/solstice-challenge/static/js/runtime~main.8e20e0b4.js"
+  }
+]);
